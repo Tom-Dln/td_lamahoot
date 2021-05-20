db32f2a5fd823c7524e14a2f84b5bcb117154dd5
Tom Delaunay <tdelaunay.lamanu@gmail.com>
2021-05-20 23:06:28 +0200

    v0.3.1

--- import.sql
+++
@@ -1,6 +0,0 @@
-LOAD DATA INFILE 'questions-answers.csv'
-    INTO TABLE `lamahoot`.questions
-    FIELDS TERMINATED BY ','
-    LINES TERMINATED BY '\n'
-    IGNORE 1 LINES
-    (culture,theme_id,type,question_name,answer_1, answer_2,answer_3,answer_4,right_answer);
